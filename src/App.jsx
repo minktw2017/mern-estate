f4c30acfe58766d4a1bb94fc21df4080969da41f
Mink ASUS <minkeiwu@gmail.com>
2024-01-13 17:12:56 +0800

    install react-router-dom and setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx"
+import SingIn from "./pages/Singin";
+import SignUp from "./pages/SignUp.jsx";
+import Profile from "./pages/Profile.jsx";
+import About from "./pages/About.jsx"
 
 export default function App() {
   return (
-    <h1
-      className='text-red-500'
-    >
-      App
-    </h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SingIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   )
 }
