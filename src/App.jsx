bf7d2d689d12de72e94a991f1c0cc734902f443d
Mink ASUS <minkeiwu@gmail.com>
2024-01-22 09:44:39 +0800

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
 import Home from "./pages/Home.jsx"
 import SingIn from "./pages/Singin";
 import SignUp from "./pages/SignUp.jsx";
 import Profile from "./pages/Profile.jsx";
-import About from "./pages/About.jsx"
+import About from "./pages/About.jsx";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SingIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SingIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
